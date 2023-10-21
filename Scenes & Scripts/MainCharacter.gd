7df013d701d44c2cb400a40b2b5c2255c11f384f
Minokawa <rikkumine@gmail.com>
2023-10-21 16:22:11 +0800

    Added more Assets and functions
    Added key functions for book and interact

--- Scenes & Scripts/MainCharacter.gd
+++ Scenes & Scripts/MainCharacter.gd
@@ -1,23 +1,31 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+
 @onready var sprite = get_node("AnimatedSprite2D")
 
+var openBook = false
+
+signal observe(activate)
+
 func _physics_process(delta):
+	# Used to process player movement
 	var direction = Vector2()
 	var collision
 	
-	direction.x = Input.get_axis("Right", "Left")
-	direction.y = Input.get_axis("Up","Down")
+	if !openBook:
+		# Player can only move if the book is not opened
+		direction.x = Input.get_axis("Right", "Left")
+		direction.y = Input.get_axis("Up","Down")
 	
 	if direction.y < 0:
-		sprite.frame = 2
+		sprite.frame = 1
 	elif direction.y > 0: 
 		sprite.frame = 0
 	elif direction.x > 0:
-		sprite.frame = 1
+		sprite.frame = 3
 	elif direction.x < 0:
-		sprite.frame = 1
+		sprite.frame = 2
 	
 	if direction:
 		velocity = direction.normalized() * SPEED
@@ -29,7 +37,15 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _on_area_2d_area_entered(area):
+	#emits if Player is in an observable flower
+	observe.emit(true)
 	print("Flower")
 
 func _on_flower_body_exited(body):
+	#emits if Player leaves the observable flower
+	observe.emit(false)
 	print("Left Flower")
+
+func _on_gui_open_book(opened):
+	#detects if the book is opened or not
+	openBook = opened
--- Scenes & Scripts/gui.gd
+++ Scenes & Scripts/gui.gd
@@ -1,11 +1,42 @@
 extends Control
 
+signal open_book(opened)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	size = get_viewport().size
+@onready var observe_btn = get_node("TextureButton")
+@onready var book = get_node("Book")
+@onready var booktxt = get_node("book_txt")
+var bookState = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _input(event):
+	# 'E' to observe
+	if event.is_action_pressed("Interact") and observe_btn.disabled == false and !bookState:
+		#observe_btn._toggled(true)
+		_on_texture_button_pressed()
+	
+	# 'Space' to open book
+	if event.is_action_pressed("Toggle_Book"):
+		if !bookState:
+			book.visible = true
+			booktxt.visible = false
+			bookState = true
+			open_book.emit(true)
+		else:
+			book.visible = false
+			booktxt.visible = true
+			bookState = false
+			open_book.emit(false)
+		
 func _process(delta):
 	pass
+
+func _on_main_character_observe(activate):
+	if activate:
+		print("Test")
+		observe_btn.disabled = false
+		observe_btn.visible = true
+	else:
+		observe_btn.disabled = true
+		observe_btn.visible = false
+
+func _on_texture_button_pressed():
+	print("Button clicked")
