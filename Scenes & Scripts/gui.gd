2215a35cf08699e75eda559db9481073b62d525f
Minokawa <rikkumine@gmail.com>
2023-10-22 08:48:37 +0800

    Added a new flower type and collision boxes

--- Scenes & Scripts/gui.gd
+++ Scenes & Scripts/gui.gd
@@ -7,6 +7,7 @@ signal open_book(opened)
 @onready var booktxt = get_node("book_txt")
 @onready var azalea = get_node("Azalea")
 @onready var flower = get_node("Random Flower")
+@onready var beautyberry = get_node("Beautyberry")
 
 var bookState = false
 var collidedObject
@@ -15,9 +16,10 @@ var collidedObject
 
 func _input(event):
 	# 'E' to observe
-	if event.is_action_pressed("Interact") and (azalea.visible or flower.visible):
+	if event.is_action_pressed("Interact") and (azalea.visible or flower.visible or beautyberry.visible):
 		azalea.visible = false
 		flower.visible = false
+		beautyberry.visible = false
 	elif event.is_action_pressed("Interact") and !observe_btn.disabled and !bookState:
 		_on_texture_button_pressed()
 
@@ -49,6 +51,8 @@ func _on_texture_button_pressed():
 
 	if collidedObject.is_in_group("Azalea") and !azalea.visible:
 		azalea.visible = true
+	elif collidedObject.is_in_group("Beautyberry") and !beautyberry.visible:
+		beautyberry.visible = true
 	elif collidedObject.is_in_group("Flower") and !flower.visible:
 		flower.visible = true
 
